34b0c37982a64f6b7ae748707015e7392b141568
Nome Exemplo <josianesteiernagel@gmail.com>
2024-10-20 18:07:49 -0300

    Mudanças nas funções para salvar imagem

--- Js/index.js
+++ Js/index.js
@@ -2,13 +2,12 @@ const livros = JSON.parse(localStorage.getItem("listaLivros")); // recupera as i
 var recentes = []; // separa e armazena informações dos recentes
 var trash = []; // separa e armazena informações de BookTrash
 
-
 const divRecentes = document.getElementById("recente"); // container para apresentar lidos recentemente
 const booktrash = document.getElementById("divBooktrash"); //container para apresentar bookTrash
 
 // função para incluir no array "recentes" os últimos 5 livros adicionados
 function separaRecentes(){
-    for(let i = 0; i<5; i++){
+    for(let i = 0; i<5; i++){ // precisa de ajuste na lógica: começar de trás para frente
         recentes.push(livros[i]);
     }
     console.log(recentes)
@@ -24,12 +23,29 @@ function separaBookTrash(){
     console.log(trash)
 }
 
-function exibeRecentes(){
-    for(let i=0; i<livros.length; i++){
-        
-    }
-}
 
 separaRecentes();
 separaBookTrash();
-console.log(livros[1].titulo);
+console.log(livros[0].titulo);
+
+// funcional, mas precisa de ajustes
+function mostrarRecentes(){
+    var imagemLivro1 = document.getElementById("capa1");
+    var imagemLivro2 = document.getElementById("capa2");
+    var imagemLivro3 = document.getElementById("capa3");
+    var imagemLivro4 = document.getElementById("capa4");
+    var imagemLivro5 = document.getElementById("capa5");
+
+    imagemLivro1.src = recentes[0].capa;
+    imagemLivro1.style.display = "block";
+    console.log(imagemLivro1);
+    
+}
+
+// testes
+function mostrarTrash(){
+    for(let i=0; i<trash.length; i++){
+        const arrayTrash = document.getElementsByClassName("info-trash")[i].value;
+    }
+}
+mostrarRecentes();
--- Js/script.js
+++ Js/script.js
@@ -1,36 +1,39 @@
-var livros = JSON.parse(localStorage.getItem("listaLivros")) || [];
+var livros = JSON.parse(localStorage.getItem("listaLivros")) || []; // carrega ou cria um array com as informações salvas no local Storage
 
-
+// Função ocorre quando o formulário com as informações do livro é enviado
 document.getElementById("form-livro").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-    const titulo = document.getElementById("titulo-livro").value;
+    event.preventDefault(); // impede que o formulário seja enviado antes de salvar as informações4
+    // constantes para armazenar as informações cadastradas
+    const titulo = document.getElementById("titulo-livro").value; 
     const autor = document.getElementById("autor-livro").value;
     const resenha = document.getElementById("resenha-livro").value;
     const genero = document.getElementById("genero-livro").value;
     const progresso = document.getElementById("progresso-livro").value;
+    // querySelector procura pelos elementos input com nome "star" e verfica se foram checadas. Utilza operador ternário para obter o valor das que forão checadas ? = if : = else
     const avaliacao = document.querySelector('input[name="star"]:checked') ? document.querySelector('input[name="star"]:checked').value : '';
-    const capa = previewImagem.src;
+    const capa = previewImagem.src; // armazena a src da imagem apresentada
 
-    let livro;
+    let livro; // variável para criar novo objeto
 
-    if (titulo && autor && genero && progresso && avaliacao && capa) {
-        
-        livro = new novoLivro(titulo, autor, resenha, genero, progresso, avaliacao, capa);
+    if (titulo && autor && genero && progresso && avaliacao && capa) { // verifica se todos os campos forão preenchidos
+        // chama a função novoLivro para criar novo objeto e armazena os valores em livro
+        livro = new novoLivro(titulo, autor, resenha, genero, progresso, avaliacao, capa); 
 
-        livros.push(livro)
-        localStorage.setItem('listaLivros', JSON.stringify(livros));
+        livros.push(livro) // adiciona as informações do objeto no array livros
+        localStorage.setItem('listaLivros', JSON.stringify(livros)); // adiciona as informações de livros no localStorage
 
-        alert("Livro salvo com sucesso!");
+        alert("Livro salvo com sucesso!"); // envia um aviso para o usuário
 
-        document.getElementById("form-livro").reset();
-        previewImagem.src = "";
-        previewImagem.style.display = "none";
-    } else {
+        document.getElementById("form-livro").reset(); // reseta as informações preenchidas
+        previewImagem.src = ""; // limpa o campo da imagem
+        previewImagem.style.display = "none"; // esconde o display da imagem
+    } else { // caso todos os campos não forem preenchidos
         alert("Por favor, preencha todos os campos.");
     }
 
 })
 
+// função para criar um objeto com as informações do formulário
 function novoLivro(titulo, autor, resenha, genero, progresso, avaliacao, capa){
     this.titulo = titulo;
     this.autor = autor;
@@ -41,20 +44,31 @@ function novoLivro(titulo, autor, resenha, genero, progresso, avaliacao, capa){
     this.capa = capa;
 }
 
-const inputImagem = document.getElementById('upload-imagem');
-const previewImagem = document.getElementById('preview-imagem');
+const inputImagem = document.getElementById('upload-imagem'); // recebe o arquivo selecionado
+const previewImagem = document.getElementById('preview-imagem'); // recebe a tag img para mostrar a imagem
 
+// Quando adicionada uma imagem, carrega a função para transformar a imagem em Base64 e apresentar ao usuário
 inputImagem.addEventListener('change', function(event) {
-    const arquivo = event.target.files[0];
-    if (arquivo) {
-        const imageUrl = URL.createObjectURL(arquivo);
-        previewImagem.src = imageUrl;
-        previewImagem.style.display = "block"; 
+    const arquivo = event.target.files[0]; // armazena a constante o arquivo selecionado pelo usuário. Files[0] para selecionar o arquivo indicado pelo usuário
+    if (arquivo) { // se existir o arquivo:
+        const reader = new FileReader(); // usado para ler o arquivo e transformar em string base64 (para que possa ser armazenada em local Storage)
+        // quando o arquivo for lido a função irá pegar o res
+        reader.onload = function() { 
+            const imagemBase64 = reader.result; // resuktado da da imagem transformada em base64
+            previewImagem.src = imagemBase64; // envia para a tag img src a imagem trsnformada
+            previewImagem.style.display = "block"; // torna o display visível
+
+            console.log(imagemBase64); // para testar :)
+        };
+
+        reader.readAsDataURL(arquivo); // lê o conteúdo do arquivo. readAsDataURL lê o arquivo e o codifica como URL base 64 - nesse caso é usada para apresentar a imagem
     }
 });
 
+// Quando clicado "descartar" limpa as informações do formulário
 document.getElementById("descartar").addEventListener("click", function(){
     document.getElementById("form-livro").reset();
     previewImagem.src = "";
         previewImagem.style.display = "none";
 })
+
