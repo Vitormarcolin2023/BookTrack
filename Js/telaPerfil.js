0c67c69038cb450c56c0ce83e5979f045336974d
Lethicia <lethiciamsm@gmail.com>
2024-10-30 12:43:04 -0300

    estrelas de avaliação na página de perfil

--- Js/telaPerfil.js
+++ Js/telaPerfil.js
@@ -1,62 +1,71 @@
-window.onload = function() {
-    lerResenhas();
+window.onload = function () {
+  lerResenhas();
 };
 
 function obterResenhas() {
-    var resenhas = localStorage.getItem("listaLivros");
-    return resenhas ? JSON.parse(resenhas) : [];
+  var resenhas = localStorage.getItem("listaLivros");
+  return resenhas ? JSON.parse(resenhas) : [];
 }
 
 function lerResenhas() {
-    var container = document.getElementById("container-resenhas");
-    var listaLivros = obterResenhas();
+  var container = document.getElementById("container-resenhas");
+  var listaLivros = obterResenhas();
 
-    container.innerHTML = '';
+  container.innerHTML = "";
 
-    listaLivros.forEach(function(livro) {
-        var resenhaDiv = document.createElement("div");
-        resenhaDiv.classList.add("resenha-item");
+  listaLivros.forEach(function (livro) {
+    var resenhaDiv = document.createElement("div");
+    resenhaDiv.classList.add("resenha-item");
 
-        var imagemDiv = document.createElement("div");
-        imagemDiv.classList.add("imagem-container");
-        
-        var imagem = document.createElement("img");
-        imagem.src = "/resource/image.png"; 
-        imagem.alt = "Imagem padrão";
-        imagem.classList.add("imagem");
-        imagemDiv.appendChild(imagem);
-        
-        var conteudoDiv = document.createElement("div");
-        conteudoDiv.classList.add("conteudo-container");
+    var imagemDiv = document.createElement("div");
+    imagemDiv.classList.add("imagem-container");
 
-        var infoDiv = document.createElement("div");
-        infoDiv.classList.add("info-container");
+    var imagem = document.createElement("img");
+    imagem.src = "/resource/image.png";
+    imagem.alt = "Imagem padrão";
+    imagem.classList.add("imagem");
+    imagemDiv.appendChild(imagem);
 
-        var nome = document.createElement("h2");
-        nome.textContent = "Maju:";
-        nome.classList.add("nome");
-        infoDiv.appendChild(nome);
+    var conteudoDiv = document.createElement("div");
+    conteudoDiv.classList.add("conteudo-container");
 
-        var novaClassificacao = document.createElement("div");
-        novaClassificacao.textContent = "Avaliação: " + livro.avaliacao;
-        novaClassificacao.classList.add("avaliacao");
-        infoDiv.appendChild(novaClassificacao);
+    var infoDiv = document.createElement("div");
+    infoDiv.classList.add("info-container");
 
-        conteudoDiv.appendChild(infoDiv);
+    var nome = document.createElement("h2");
+    nome.textContent = "Maju:";
+    nome.classList.add("nome");
+    infoDiv.appendChild(nome);
 
-        var resenhaConteudoDiv = document.createElement("div");
-        resenhaConteudoDiv.classList.add("resenha-container");
-        
-        var novaResenha = document.createElement("p");
-        novaResenha.textContent = livro.resenha;
-        novaResenha.classList.add("resenhas");
-        resenhaConteudoDiv.appendChild(novaResenha);
+    var novaClassificacao = document.createElement("div");
+    novaClassificacao.classList.add("avaliacao");
 
-        conteudoDiv.appendChild(resenhaConteudoDiv);
+    let avaliacao = livro.avaliacao;
 
-        resenhaDiv.appendChild(imagemDiv);
-        resenhaDiv.appendChild(conteudoDiv);
+    for (let i = 1; i <= 5; i++) {
+      let starLabel = document.createElement("span");
+      starLabel.classList.add("star-label");
+      starLabel.textContent = i <= avaliacao ? "★" : "☆";
+      novaClassificacao.appendChild(starLabel);
+    }
 
-        container.appendChild(resenhaDiv);
-    });
+    infoDiv.appendChild(novaClassificacao);
+
+    conteudoDiv.appendChild(infoDiv);
+
+    var resenhaConteudoDiv = document.createElement("div");
+    resenhaConteudoDiv.classList.add("resenha-container");
+
+    var novaResenha = document.createElement("p");
+    novaResenha.textContent = livro.resenha;
+    novaResenha.classList.add("resenhas");
+    resenhaConteudoDiv.appendChild(novaResenha);
+
+    conteudoDiv.appendChild(resenhaConteudoDiv);
+
+    resenhaDiv.appendChild(imagemDiv);
+    resenhaDiv.appendChild(conteudoDiv);
+
+    container.appendChild(resenhaDiv);
+  });
 }
